dbeda4fcc924fda6a4131197bbeea0620d8a0121
Luna <24762027+SpaxscE@users.noreply.github.com>
2024-07-31 12:29:38 +0200

    force lvs default settings for gamemode

--- gamemodes/lvs/gamemode/shared.lua
+++ gamemodes/lvs/gamemode/shared.lua
@@ -3,7 +3,17 @@ GM.Author = "Luna"
 GM.Email = "juliewerding@gmx.de"
 GM.Website = "https://discord.gg/BeVtn7uwNH"
 
+DeriveGamemode( "sandbox" )
+
 function GM:Initialize()
 	print("runs")
 end
 
+hook.Add( "LVS:Initialize", "lvs_gamemode_force_convars", function()
+	LVS.HudForceDefault = true
+	LVS.FreezeTeams = true
+	LVS.TeamPassenger = true
+
+	cvars.RemoveChangeCallback( "lvs_freeze_teams", "lvs_freezeteams_callback" )
+	cvars.RemoveChangeCallback( "lvs_teampassenger", "lvs_teampassenger_callback" )
+end )
