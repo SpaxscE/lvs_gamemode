80fb21833f075f57b8ae5b4d5e8bf9d408e066da
Luna <24762027+SpaxscE@users.noreply.github.com>
2024-07-31 12:40:09 +0200

    aparently, the gamemode loads last, lvs base loads first.

--- gamemodes/lvs/gamemode/shared.lua
+++ gamemodes/lvs/gamemode/shared.lua
@@ -6,14 +6,12 @@ GM.Website = "https://discord.gg/BeVtn7uwNH"
 DeriveGamemode( "sandbox" )
 
 function GM:Initialize()
-	print("runs")
-end
+print("runs")
 
-hook.Add( "LVS:Initialize", "lvs_gamemode_force_convars", function()
 	LVS.HudForceDefault = true
 	LVS.FreezeTeams = true
 	LVS.TeamPassenger = true
 
 	cvars.RemoveChangeCallback( "lvs_freeze_teams", "lvs_freezeteams_callback" )
 	cvars.RemoveChangeCallback( "lvs_teampassenger", "lvs_teampassenger_callback" )
-end )
+end
