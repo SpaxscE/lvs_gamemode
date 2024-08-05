433a81e48c1dc33de133bf356d6192810c8fcc08
Luna <24762027+SpaxscE@users.noreply.github.com>
2024-08-05 20:25:24 +0200

    add GM:GetTeamColor()

--- gamemodes/lvs/gamemode/cl_init.lua
+++ gamemodes/lvs/gamemode/cl_init.lua
@@ -3,3 +3,18 @@ include( "cl_hud.lua" )
 include( "cl_join.lua" )
 include( "buymenu/cl_buymenu.lua" )
 include( "buymenu/cl_buymenu_button.lua" )
+
+local ColFriend = Color(0,127,255,255)
+local ColEnemy = Color(255,0,0,255)
+
+function GM:GetTeamColor( ent )
+
+	local team = TEAM_UNASSIGNED
+	if ( ent.Team ) then team = ent:Team() end
+
+	if team == TEAM_SPECTATOR or not ent.lvsGetAITeam then
+		return GAMEMODE:GetTeamNumColor( team )
+	end
+
+	return (LocalPlayer():lvsGetAITeam() == ent:lvsGetAITeam()) and ColFriend or ColEnemy
+end
